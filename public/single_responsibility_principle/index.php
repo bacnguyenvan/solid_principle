e89fd986d8c038a857c256f97323660427412dcf
bacETA <bacnv@ethan-tech.com>
2023-08-25 08:40:08 +0700

    single responsibility principle

---
+++ public/single_responsibility_principle/index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+    - It states that a class should have A SINGLE RESPONSIBILITY.
+    - A good practice to follow is to list the responsibility of the class in the comment doc-block.
+    - This way you can remind yourself and others the purpose of this class and try to keep the responsibility minimal.
+*/
+
+class EmptyGarden
+{
+    private $width;
+    private $height;
+
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function items()
+    {
+        $numberOfSpots = ceil($this->width * $this->height);
+
+        return array_fill(0, $numberOfSpots, 'handful of dirt');
+    }
+}
