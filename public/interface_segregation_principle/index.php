3fa2bb92a7282c143a37255b9468f8269f3b0325
bacDev <bacb4tvk@gmail.com>
2023-08-27 20:52:39 +0700

    add interface segregation principle

---
+++ public/interface_segregation_principle/index.php
@@ -0,0 +1,7 @@
+<?php
+/*
+-   ISP states that no client should be forced to depend on methods it does not use.
+
+interface GardenInterface implements GrơableInterface, WeedableInterface, ...
+
+*/
